701eaa2934afd5ea3a81911df936853b0a800151
“Greyless” <singhallakshaya@gmail.com>
2023-02-28 14:59:27 +0530

    Added MCPWM for esp idf 5.0

---
+++ Firmware/components/esp-idf-drv8305/include/drv8305.h
@@ -0,0 +1,200 @@
+#ifndef DRV8305_H
+#define DRV8305_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+#define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
+#define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
+
+#define DRV8305_STATUS_01_REG_ADDR 0x01
+#define DRV8305_STATUS_02_REG_ADDR 0x02
+#define DRV8305_STATUS_03_REG_ADDR 0x03
+#define DRV8305_STATUS_04_REG_ADDR 0x04
+#define DRV8305_CONTROL_05_REG_ADDR 0x05
+#define DRV8305_CONTROL_06_REG_ADDR 0x06
+#define DRV8305_CONTROL_07_REG_ADDR 0x07
+#define DRV8305_CONTROL_09_REG_ADDR 0x09
+#define DRV8305_CONTROL_0A_REG_ADDR 0x0A
+#define DRV8305_CONTROL_0B_REG_ADDR 0x0B
+#define DRV8305_CONTROL_0C_REG_ADDR 0x0C
+
+// struct which holds data for SPI driver
+typedef struct drv8305_t_ {
+    spi_device_interface_config_t spi_cfg;
+    spi_device_handle_t spi_dev;
+    gpio_num_t DRV_EN_GATE_pin;
+    gpio_num_t DRV_N_FAULT_pin;
+    gpio_num_t DRV_MISO_SDO_pin;
+    gpio_num_t DRV_MOSI_SDI_pin;
+    gpio_num_t DRV_SCLK_pin;
+    gpio_num_t DRV_SCS_pin;
+    spi_host_device_t spi_host;
+    uint32_t max_spi_clockspeed;
+} drv8305_t;
+
+// Structs defining the registers on drv8305
+typedef struct drv8305_status_01_reg_t_
+{
+    uint8_t OTW            :1 ; // Bits 0
+    uint8_t TEMP_FLAG3     :1 ; // Bits 1
+    uint8_t TEMP_FLAG2     :1 ; // Bits 2
+    uint8_t TEMP_FLAG1     :1 ; // Bits 3
+    uint8_t VCPH_UVFL      :1 ; // Bits 4
+    uint8_t VDS_STATUS     :1 ; // Bits 5
+    uint8_t PVDD_OVFL      :1 ; // Bits 6
+    uint8_t PVDD_UVFL      :1 ; // Bits 7
+    uint8_t TEMP_FLAG4     :1 ; // Bits 8
+    uint8_t STAT01_RSV1    :1 ; // Bits 9
+    uint8_t FAULT          :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_status_01_reg_t;
+
+typedef struct drv8305_status_02_reg_t_
+{
+    uint8_t SNS_A_OCP      :1 ; // Bits 0,
+    uint8_t SNS_B_OCP      :1 ; // Bits 1
+    uint8_t SNS_C_OCP      :1 ; // Bits 2
+    uint8_t STAT02_RSV1    :2 ; // Bits 3:4
+    uint8_t FETLC_VDS      :1 ; // Bits 5
+    uint8_t FETHC_VDS      :1 ; // Bits 6
+    uint8_t FETLB_VDS      :1 ; // Bits 7
+    uint8_t FETHB_VDS      :1 ; // Bits 8
+    uint8_t FETLA_VDS      :1 ; // Bits 9
+    uint8_t FETHA_VDS      :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_status_02_reg_t;
+
+typedef struct drv8305_status_03_reg_t_
+{
+    uint8_t VCPH_OVLO_ABS  :1 ; // Bits 0
+    uint8_t VCPH_OVLO      :1 ; // Bits 1
+    uint8_t VCPH_UVLO2     :1 ; // Bits 2
+    uint8_t STAT03_RSV1    :1 ; // Bits 3
+    uint8_t VCP_LSD_UVLO2  :1 ; // Bits 4
+    uint8_t AVDD_UVLO      :1 ; // Bits 5
+    uint8_t VREG_UV        :1 ; // Bits 6
+    uint8_t STAT03_RSV2    :1 ; // Bits 7
+    uint8_t OTSD           :1 ; // Bits 8
+    uint8_t WD_FAULT       :1 ; // Bits 9
+    uint8_t PVDD_UVLO2     :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_status_03_reg_t;
+
+typedef struct drv8305_status_04_reg_t_
+{
+    uint8_t STAT04_RSV1    :5 ; // Bits 0:4
+    uint8_t FETLC_VGS      :1 ; // Bits 5
+    uint8_t FETHC_VGS      :1 ; // Bits 6
+    uint8_t FETLB_VGS      :1 ; // Bits 7
+    uint8_t FETHB_VGS      :1 ; // Bits 8
+    uint8_t FETLA_VGS      :1 ; // Bits 9
+    uint8_t FETHA_VGS      :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_status_04_reg_t;
+
+typedef struct drv8305_control_05_reg_t_
+{
+    uint8_t IDRIVEP_HS     :4 ; // Bits 0:3
+    uint8_t IDRIVEN_HS     :4 ; // Bits 3:7
+    uint8_t TDRIVEN        :2 ; // Bits 8:9
+    uint8_t STAT05_RSV1    :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_control_05_reg_t;
+
+typedef struct drv8305_control_06_reg_t_
+{
+    uint8_t IDRIVEP_LS     :4 ; // Bits 0:3
+    uint8_t IDRIVEN_LS     :4 ; // Bits 3:7
+    uint8_t TDRIVEP        :2 ; // Bits 8:9
+    uint8_t STAT06_RSV1    :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_control_06_reg_t;
+
+typedef struct drv8305_control_07_reg_t_
+{
+    uint8_t TVDS           :2 ; // Bits 0:1
+    uint8_t TBLANK         :2 ; // Bits 2:3
+    uint8_t DEAD_TIME      :3 ; // Bits 4:6
+    uint8_t PWM_MODE1       :1 ; // Bits 7:8
+    uint8_t PWM_MODE2       :1 ;
+    uint8_t COMM_OPTION    :1 ; // Bits 9
+    uint8_t STAT07_RSV1    :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_control_07_reg_t;
+
+typedef struct drv8305_reserved_08_reg_t_
+{
+    uint16_t STAT08_RSV    :11 ; // Bits 0:10
+} __attribute__((packed, aligned(2))) drv8305_reserved_08_reg_t;
+
+typedef struct drv8305_control_09_reg_t_
+{
+    uint8_t SET_VCPH_UV    :1 ; // Bits 0
+    uint8_t CLR_FLTS       :1 ; // Bits 1
+    uint8_t SLEEP          :1 ; // Bits 2
+    uint8_t WD_EN          :1 ; // Bits 3
+    uint8_t DIS_SNS_OCP    :1 ; // Bits 4
+    uint8_t WD_DLY         :2 ; // Bits 5:6
+    uint8_t EN_SNS_CLAMP   :1 ; // Bits 7
+    uint8_t DIS_GDRV_FAULT :1 ; // Bits 8
+    uint8_t DIS_PVDD_UVLO2 :1 ; // Bits 9
+    uint8_t FLIP_OTSD      :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_control_09_reg_t;
+
+typedef struct drv8305_control_0A_reg_t_
+{
+    uint8_t GAIN_CS1      :2 ; // Bits 0:1
+    uint8_t GAIN_CS2      :2 ; // Bits 2:3
+    uint8_t GAIN_CS3      :2 ; // Bits 4:5
+    uint8_t CS_BLANK      :2 ; // Bits 6:7
+    uint8_t DC_CAL_CH1    :1 ; // Bits 8
+    uint8_t DC_CAL_CH2    :1 ; // Bits 9
+    uint8_t DC_CAL_CH3    :1 ; // Bits 10
+
+} __attribute__((packed, aligned(2))) drv8305_control_0A_reg_t;
+
+typedef struct drv8305_control_0B_reg_t_
+{
+    uint8_t VREG_UV_LEVEL  :2 ; // Bits 0:1
+    uint8_t DIS_VREG_PWRGD :1 ; // Bits 2
+    uint8_t SLEEP_DLY      :2 ; // Bits 3:4
+    uint8_t STAT11_RSV1    :3 ; // Bits 5:7
+    uint8_t VREF_SCALE     :2 ; // Bits 8:9
+    uint8_t STAT11_RSV2    :1 ; // Bits 10
+} __attribute__((packed, aligned(2))) drv8305_control_0B_reg_t;
+
+typedef struct drv8305_control_0C_reg_t_
+{
+    uint8_t VDS_MODE      :3 ; // Bits 0:2
+    uint8_t VDS_LEVEL     :5 ; // Bits 3:7
+    uint8_t STAT12_RSV1   :3 ; // Bits 8:10
+
+} __attribute__((packed, aligned(2))) drv8305_control_0C_reg_t;
+
+esp_err_t drv8305_init(drv8305_t *dev);
+esp_err_t drv8305_write_register(drv8305_t *dev, uint8_t register_address, uint16_t value);
+esp_err_t drv8305_read_register(drv8305_t *dev, uint8_t register_address, uint16_t *value);
+
+esp_err_t drv8305_read_status_01_register(drv8305_t *dev, drv8305_status_01_reg_t *value);
+esp_err_t drv8305_read_status_02_register(drv8305_t *dev, drv8305_status_02_reg_t *value);
+esp_err_t drv8305_read_status_03_register(drv8305_t *dev, drv8305_status_03_reg_t *value);
+esp_err_t drv8305_read_status_04_register(drv8305_t *dev, drv8305_status_04_reg_t *value);
+
+esp_err_t drv8305_read_control_05_register(drv8305_t *dev, drv8305_control_05_reg_t *value);
+esp_err_t drv8305_read_control_06_register(drv8305_t *dev, drv8305_control_06_reg_t *value);
+esp_err_t drv8305_read_control_07_register(drv8305_t *dev, drv8305_control_07_reg_t *value);
+esp_err_t drv8305_read_control_09_register(drv8305_t *dev, drv8305_control_09_reg_t *value);
+esp_err_t drv8305_read_control_0A_register(drv8305_t *dev, drv8305_control_0A_reg_t *value);
+esp_err_t drv8305_read_control_0B_register(drv8305_t *dev, drv8305_control_0B_reg_t *value);
+esp_err_t drv8305_read_control_0C_register(drv8305_t *dev, drv8305_control_0C_reg_t *value);
+
+esp_err_t drv8305_write_control_05_register(drv8305_t *dev, drv8305_control_05_reg_t value);
+esp_err_t drv8305_write_control_06_register(drv8305_t *dev, drv8305_control_06_reg_t value);
+esp_err_t drv8305_write_control_07_register(drv8305_t *dev, drv8305_control_07_reg_t value);
+esp_err_t drv8305_write_control_09_register(drv8305_t *dev, drv8305_control_09_reg_t value);
+esp_err_t drv8305_write_control_0A_register(drv8305_t *dev, drv8305_control_0A_reg_t value);
+esp_err_t drv8305_write_control_0B_register(drv8305_t *dev, drv8305_control_0B_reg_t value);
+esp_err_t drv8305_write_control_0C_register(drv8305_t *dev, drv8305_control_0C_reg_t value);
+
+#endif
--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -1,5 +1,5 @@
 #include "driver/gpio.h"
-#include "driver/mcpwm.h"
+#include "driver/mcpwm_prelude.h"
 #include "driver/spi_master.h"
 #include "drv8305.h"
 #include "esp_err.h"
@@ -11,11 +11,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define BLDC_MCPWM_TIMER_RESOLUTION_HZ 10000000 // 10MHz, 1 tick = 0.1us
+#define BLDC_MCPWM_PERIOD              500      // 50us, 20KHz
+#define BLDC_SPEED_UPDATE_PERIOD_US    200000   // 200ms
+
 #define PWM_H_A 17
 #define PWM_H_B 5
 #define PWM_H_C 18
-
-#define PWM_FREQUENCY 20000
 
 #define MISO_PIN 12
 #define MOSI_PIN 13
@@ -24,14 +26,12 @@
 #define DRV8305_EN 2
 #define DRV8305_NFAULT 4
 
+#define BLDC_MCPWM_GEN_INDEX_HIGH 0
+#define BLDC_MCPWM_GEN_INDEX_LOW  1
+
 static const char TAG[] = "bldc mcpwm";
 
 void init_gpio() {
-
-  // MCPWM
-  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, PWM_H_A);
-  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, PWM_H_B);
-  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM2A, PWM_H_C);
 
   drv8305_t dev = {.DRV_EN_GATE_pin = DRV8305_EN,
                    .DRV_N_FAULT_pin = DRV8305_NFAULT,
@@ -47,57 +47,236 @@ void init_gpio() {
 
   drv8305_control_07_reg_t pwm_mode_config;
   drv8305_read_control_07_register(&dev, &pwm_mode_config);
-  ESP_LOGI(TAG, "INITIAL VALUE %x", pwm_mode_config.PWM_MODE);
-  pwm_mode_config.PWM_MODE = 0b01;
+  ESP_LOGI(TAG, "INITIAL VALUE %x", pwm_mode_config.PWM_MODE1);
+  ESP_LOGI(TAG, "INITIAL VALUE %x", pwm_mode_config.PWM_MODE2);
+  pwm_mode_config.PWM_MODE1 = 0b0;
+  pwm_mode_config.PWM_MODE2 = 0b1;
   ESP_ERROR_CHECK(drv8305_write_control_07_register(&dev, pwm_mode_config));
 
   drv8305_read_control_07_register(&dev, &pwm_mode_config);
-  ESP_LOGI(TAG, "FINAL VALUE %x", pwm_mode_config.PWM_MODE);
+  ESP_LOGI(TAG, "FINAL VALUE %x", pwm_mode_config.PWM_MODE1);
+  ESP_LOGI(TAG, "FINAL VALUE %x", pwm_mode_config.PWM_MODE2);
 }
 
 void mcpwm_config() {
-  mcpwm_config_t pwm_config;
-  pwm_config.frequency = PWM_FREQUENCY; // frequency = 20000Hz
-  pwm_config.cmpr_a = 0;                // duty cycle of PWMxA = 50.0%
-  pwm_config.cmpr_b = 0;                // duty cycle of PWMxB = 50.0%
-  pwm_config.counter_mode =
-      MCPWM_UP_DOWN_COUNTER;                // Up-down counter (triangle wave)
-  pwm_config.duty_mode = MCPWM_DUTY_MODE_0; // Active HIGH
-  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0,
-             &pwm_config); // Configure PWM0A & PWM0B with above settings
-  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1,
-             &pwm_config); // Configure PWM0A & PWM0B with above settings
-  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_2,
-             &pwm_config); // Configure PWM0A & PWM0B with above settings
+  
+  // gpio_config_t drv_en_config = {
+  //       .mode = GPIO_MODE_OUTPUT,
+  //       .pin_bit_mask = 1ULL << DRV8305_EN,
+  //   };
+  // ESP_ERROR_CHECK(gpio_config(&drv_en_config));
+  // gpio_set_level(DRV8305_EN, 1);
 
-  mcpwm_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
-  mcpwm_stop(MCPWM_UNIT_0, MCPWM_TIMER_1);
-  mcpwm_stop(MCPWM_UNIT_0, MCPWM_TIMER_2);
 
-  mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_0);
-  mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_1);
-  mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_2);
+  // mcpwm_timer_handle_t timer = NULL;
+  // mcpwm_timer_config_t timer_config = {
+  //       .group_id = 0,
+  //       .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+  //       .resolution_hz = BLDC_MCPWM_TIMER_RESOLUTION_HZ,
+  //       .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+  //       .period_ticks = BLDC_MCPWM_PERIOD,
+  //   };
 
-  mcpwm_sync_enable(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_SELECT_SYNC0, 0);
-  mcpwm_sync_enable(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_SELECT_SYNC0, 0);
-  mcpwm_sync_enable(MCPWM_UNIT_0, MCPWM_TIMER_2, MCPWM_SELECT_SYNC0, 0);
+  // ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+  // mcpwm_oper_handle_t operators[3];
+  // mcpwm_operator_config_t operator_config = {
+  //       .group_id = 0,
+  //   };
+  // for (int i = 0; i < 3; i++) {
+  //       ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &operators[i]));
+  // }
+
+  // ESP_LOGI(TAG, "Connect operators to the same timer");
+  // for (int i = 0; i < 3; i++) {
+  //     ESP_ERROR_CHECK(mcpwm_operator_connect_timer(operators[i], timer));
+  // }
+
+
+  // mcpwm_cmpr_handle_t comparators[3];
+  // mcpwm_comparator_config_t compare_config = {
+  //      .flags.update_cmp_on_tez = true,
+  //   };
+
+  // for (int i = 0; i < 3; i++) {
+  //       ESP_ERROR_CHECK(mcpwm_new_comparator(operators[i], &compare_config, &comparators[i]));
+  //       // set compare value to 0, we will adjust the speed in a period timer callback
+  //       ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], 0));
+  //   }
+
+  // mcpwm_gen_handle_t generators[3] = {};
+  // mcpwm_generator_config_t gen_config = {};
+  // const int gen_gpios[3] = {PWM_H_A, PWM_H_B, PWM_H_C};
+  // for (int i = 0; i < 3; i++) {
+  //       gen_config.gen_gpio_num = gen_gpios[i];
+  //       ESP_ERROR_CHECK(mcpwm_new_generator(operators[i], &gen_config, &generators[i]));
+        
+  //   }
+
+  // ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+  
 }
 
 void app_main() {
+  
+  //mcpwm_config();]
+  
+  
+
+  // gpio_config_t drv_en_config = {
+  //       .mode = GPIO_MODE_OUTPUT,
+  //       // .pull_up_en = GPIO_PULLUP_DISABLE,
+  //       // .pull_down_en = GPIO_PULLDOWN_DISABLE,
+  //       .pin_bit_mask = 1ULL << DRV8305_EN,
+  //   };
+  // ESP_ERROR_CHECK(gpio_config(&drv_en_config));
+  // gpio_set_level(DRV8305_EN, 1);
+
   init_gpio();
-  mcpwm_config();
+
+  mcpwm_timer_handle_t timer;
+  mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = BLDC_MCPWM_TIMER_RESOLUTION_HZ,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+        .period_ticks = BLDC_MCPWM_PERIOD,
+    };
+
+
+
+  ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+  ESP_LOGI(TAG, "Create over current fault detector");
+  mcpwm_fault_handle_t over_cur_fault = NULL;
+  mcpwm_gpio_fault_config_t gpio_fault_config = {
+        .gpio_num = DRV8305_EN,
+        .group_id = 0,
+        .flags.active_level = 0, // low level means fault, refer to DRV8302 datasheet
+        .flags.pull_up = true,   // internally pull up
+    };
+    ESP_ERROR_CHECK(mcpwm_new_gpio_fault(&gpio_fault_config, &over_cur_fault));
+
+  
+  
+
+  mcpwm_oper_handle_t operators[3];
+  mcpwm_operator_config_t operator_config = {
+        .group_id = 0,
+    };
+  for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &operators[i]));
+  }
+
+  ESP_LOGI(TAG, "Connect operators to the same timer");
+  for (int i = 0; i < 3; i++) {
+      ESP_ERROR_CHECK(mcpwm_operator_connect_timer(operators[i], timer));
+  }
+
+
+  mcpwm_cmpr_handle_t comparators[3];
+  mcpwm_comparator_config_t compare_config = {
+       .flags.update_cmp_on_tez = true,
+       //.flags.update_cmp_on_sync = true,
+    };
+
+  for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_new_comparator(operators[i], &compare_config, &comparators[i]));
+        // set compare value to 0, we will adjust the speed in a period timer callback
+        //ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], 0));
+    }
+
+  mcpwm_gen_handle_t generators[3] = {};
+  mcpwm_generator_config_t gen_config = {};
+  const int gen_gpios[3] = {PWM_H_A, PWM_H_B, PWM_H_C};
+  for (int i = 0; i < 3; i++) {
+        gen_config.gen_gpio_num = gen_gpios[i];
+        ESP_ERROR_CHECK(mcpwm_new_generator(operators[i], &gen_config, &generators[i]));
+        
+    }
+
+  // gpio_set_level(DRV8305_EN, 1);
+
+
+  ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+  ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer,MCPWM_TIMER_START_NO_STOP));
+  
+        
+  
+
+  // mcpwm_sync_handle_t gpio_sync_source = NULL;
+  // mcpwm_gpio_sync_src_config_t gpio_sync_config = {
+  //       .group_id = 0,              // GPIO fault should be in the same group of the above timers
+  //       .gpio_num = DRV8305_NFAULT,
+  //       .flags.pull_down = true,
+  //       .flags.active_neg = false,  // by default, a posedge pulse can trigger a sync event
+  //   };
+  // ESP_ERROR_CHECK(mcpwm_new_gpio_sync_src(&gpio_sync_config, &gpio_sync_source));
+
+  // mcpwm_timer_sync_phase_config_t sync_phase_config = {
+  //       .count_value = 0,                      // sync phase: target count value
+  //       .direction = MCPWM_TIMER_DIRECTION_UP, // sync phase: count direction
+  //       .sync_src = gpio_sync_source,          // sync source
+  //   };
+  // for (int i = 0; i < 3; i++) {
+  //       ESP_ERROR_CHECK(mcpwm_timer_set_phase_on_sync(timer[i], &sync_phase_config));
+  //   }
+  
+  int pwm[3][3] = {{0, 15, 30}, {30, 0, 15}, {15, 30 , 0}}; 
 
   // PWM values to make it spin
-  int pwm[3][3] = {{0, 40, 80}, {80, 40, 0}, {40, 80, 0}};
+  for (int i = 0; i < 3; i++) {
+        // ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer,MCPWM_TIMER_START_STOP_FULL));
+        mcpwm_comparator_set_compare_value(comparators[0], pwm[0][i]);
+        mcpwm_comparator_set_compare_value(comparators[1], pwm[1][i]);
+        mcpwm_comparator_set_compare_value(comparators[2], pwm[2][i]);
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[0],
+                        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH),
+                        MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[0],
+                        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[0], MCPWM_GEN_ACTION_LOW),
+                        MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[1],
+                        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH),
+                        MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[1],
+                        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[1], MCPWM_GEN_ACTION_LOW),
+                        MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[2],
+                        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH),
+                        MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[2],
+                        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[2], MCPWM_GEN_ACTION_LOW),
+                        MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
 
-  // SPIN motor with reasonable speed, adjust delay accordingly
-  for (size_t i = 1; i <= 3; i++) {
-    mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, pwm[0][i % 3]);
-    mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, pwm[1][i % 3]);
-    mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_2, MCPWM_OPR_A, pwm[2][i % 3]);
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-    if (i == 3) {
-      i = 0;
-    }
+        vTaskDelay(50/ portTICK_PERIOD_MS);
+
+        if (i == 2) {
+           i = 0;
+        }
   }
+
+        // mcpwm_comparator_set_compare_value(comparators[0], 250);
+        // mcpwm_comparator_set_compare_value(comparators[1], 250);
+        // mcpwm_comparator_set_compare_value(comparators[2], 250);
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[0],
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_LOW),
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[0],
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[0], MCPWM_GEN_ACTION_HIGH),
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[1],
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_LOW),
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[1],
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[1], MCPWM_GEN_ACTION_HIGH),
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_timer_event(generators[2],
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_LOW),
+        //                 MCPWM_GEN_TIMER_EVENT_ACTION_END()));
+        // ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[2],
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[2], MCPWM_GEN_ACTION_HIGH),
+        //                 MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+        
+
 }
